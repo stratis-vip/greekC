ada45ef100f17bf9600f40f7d4adc4473e582448
stratis <stratis.vip@gmail.com>
2018-07-27 12:19:26 +0300

    small changes

--- lesson13/domes.c
+++ lesson13/domes.c
@@ -2,27 +2,35 @@
 // Created by Στρατής Χριστοδούλου on 26/7/18.
 //
 #include <stdio.h>
+#include <stdlib.h>
 #include "../includes/mio.h"
 
 #define STRSIZE 80
 
 typedef struct {
-    char onoma[STRSIZE];
-    char diefthinsi[STRSIZE];
-    char arithmos[STRSIZE];
-    char nomos[STRSIZE];
+    char *onoma;
+    char *diefthinsi;
+    char *arithmos;
+    char *nomos;
 } RECORD;
 
 void read_record(RECORD *rec);
 
 void print_record(RECORD rec);
 
+void init_record(RECORD *rec);
+
+void free_record(RECORD *rec);
+
 int main() {
 
     RECORD rec;
 
+    init_record(&rec);
+    
     read_record(&rec);
     print_record(rec);
+    free_record(&rec);
 
 }
 
@@ -43,5 +51,38 @@ void read_record(RECORD *rec) {
 void print_record(RECORD rec) {
     printf("%s\n%s %s\n%s",
     rec.onoma, rec.diefthinsi, rec.arithmos, rec.nomos);
+
+}
+
+void init_record(RECORD *rec) {
+    rec->onoma = malloc(sizeof(char)*STRSIZE);
+    if (!rec->onoma){
+        printf("Αδυναμία προσάρτησης μνήμης!\n");
+        exit(0);
+    }
+    rec->arithmos = malloc(sizeof(char)*STRSIZE);
+    if (!rec->arithmos){
+        printf("Αδυναμία προσάρτησης μνήμης!\n");
+        exit(0);
+    }
+    rec->diefthinsi = malloc(sizeof(char)*STRSIZE);
+    if (!rec->diefthinsi){
+        printf("Αδυναμία προσάρτησης μνήμης!\n");
+        exit(0);
+    }
+    rec->nomos = malloc(sizeof(char)*STRSIZE);
+    if (!rec->nomos){
+        printf("Αδυναμία προσάρτησης μνήμης!\n");
+        exit(0);
+    }
+}
+
+void free_record(RECORD *rec) {
+
+    free (rec->nomos);
+    free (rec->diefthinsi);
+    free (rec->onoma);
+    free (rec->arithmos);
+
 
 }
