979e76fa651fdc48a63d62d8163ba2af81c15acf
stratis <stratis.vip@gmail.com>
2018-07-27 18:22:01 +0300

    final 1

---
+++ lesson13/records.c
@@ -0,0 +1,5 @@
+//
+// Created by Στρατής Χριστοδούλου on 27/7/18.
+//
+
+#include "records.h"
---
+++ lesson13/records.h
@@ -0,0 +1,8 @@
+//
+// Created by Στρατής Χριστοδούλου on 27/7/18.
+//
+
+#ifndef LESSON13_RECORDS_H
+#define LESSON13_RECORDS_H
+
+#endif //LESSON13_RECORDS_H
